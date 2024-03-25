095c11750f7aa89c46ac2fc4d7cb0b0788531cdd
CarlosRussell13 <133693000+CarlosRussell13@users.noreply.github.com>
2024-03-25 20:25:52 +0000

    hello-react

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey Mr. Siarto, Thanks for reviewing my work.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Have a good day.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
