c0efe0c5b881098930c38279ddb1c8de28392229
CarlosRussell13 <133693000+CarlosRussell13@users.noreply.github.com>
2024-04-01 16:20:44 +0000

    10.5 assignment update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
-import logo from './logo.svg';
+import logo from './baseball.webp';
 import './App.css';
+
+const Players = [
+  {id: 1, title: 'JD Martinez', isAvailable: true},
+  {id: 2, title: 'Miguel Cabrera', isAvailable: true},
+  {id: 3, title: 'Aaron Judge', isAvailable: true}
+]
+
+const Stats= {
+  PlayerName:'Miguel Cabrera',
+  PlayerNumber:'24',
+  HR: '511',
+  image:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.mlb.com%2Fplayer%2Fmiguel-cabrera-408234&psig=AOvVaw00qFhy-GUWMnEuzsVPjcyq&ust=1712074182608000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNjkmr2zoYUDFQAAAAAdAAAAABAE',
+  width:'130',
+  height:'185'
+};
+
+function Statspot(){
+  return(
+    <div>
+      <h2>{stats.PlayerName}({book.published})</h2>
+      <p>Number {stats.PlayerNumber}</p>
+      <img className='StatImage' src={stats.image} alt={stats.title + ' cover'} style={{width: stats.width, height: stats.height}}/>
+    </div>
+  );
+}
+
+function Playerdata(){
+const listPlay = Players.map(Play =>
+  <li key={Play.id} style={{color: Play.isAvailable ? 'white':'black'}}>
+    {play.title}
+  </li>
+);
+return(
+  <ul>{listPlay}</ul>
+);
+}
 
 function App() {
   return (
@@ -17,6 +53,8 @@ function App() {
         >
           Have a good day.
         </a>
+        <statspot/>
+        <playerdata/>
       </header>
     </div>
   );
